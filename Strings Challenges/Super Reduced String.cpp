d85d4b9647fb30b89e2d33f22ed5cc697444106e
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-19 01:40:30 +0600

    Update Super Reduced String.cpp

--- Strings Challenges/Super Reduced String.cpp
+++ Strings Challenges/Super Reduced String.cpp
@@ -1,1 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
 
+int main() {
+  string s;
+  cin >> s;
+  int ans = 1;
+  for (int i = 0; i < s.size() ; i++){
+  	if (s[i] >= 65 && s[i] <= 90) ans++;
+  }
+  cout << ans <<"\n";
+  return 0;
+}
